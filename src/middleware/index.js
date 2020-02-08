1b890dffb62cb712a9802cca49bb7a17e4b0859f
Jonathan Gravato <jonathangravato@jonathans-mbp.home>
2020-02-08 10:57:36 -0500

    adding files from latest refactor of initial project

---
+++ src/middleware/index.js
@@ -0,0 +1,10 @@
+import cheker from './checker'
+import logger from './logger'
+import thunk from 'redux-thunk'
+import { applyMiddleware } from 'redux'
+
+export default applyMiddleware(
+    thunk,
+    checker,
+    logger,
+)
